ee878b4a996d51d8ba2bf47fe5709a03314d2eab
Buse <109986380+irembuseozkose@users.noreply.github.com>
2023-04-01 00:10:48 +0300

    Create average_speed_calculator.c

---
+++ average_speed_calculator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void)
+{
+    float gallons,miles,totalGallons=0.0,totalMiles=0.0,average;
+    printf("Enter the gallons used(-1 to end):");
+    scanf("%f",&gallons);
+
+    while (gallons!=-1.0){
+        printf("Enter miles driven");
+        scanf("%f",&miles);
+        totalGallons = totalGallons + gallons;
+        totalMiles=totalMiles + miles;
+        printf("The miles / gallon for this tank was %f\n",miles/gallons);
+        printf("Enter the gallons used(-1 to end):");
+        scanf("%f",&gallons);
+
+    }
+    average= totalMiles/totalGallons;
+    printf("The overall avarage miles/gallons was %f",average);
+    return 0;
+}
+// Homework for the Introduction to Computer Programming lesson
+//C: How to Program by Paul and Harvey Deitel page chapter3 page89 question 3.17
