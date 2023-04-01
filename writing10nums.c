08ae01b4ecd19f25d409f41073890e551c6913ae
Buse <109986380+irembuseozkose@users.noreply.github.com>
2023-04-01 06:17:12 +0300

    Create writing10nums.c

---
+++ writing10nums.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int x;
+
+    for (x = 1; x <= 10; x++) {
+        printf("%d   ", x);
+    }
+
+    return 0;
+}
+// Homework for the Introduction to Computer Programming lesson
+//C: How to Program by Paul and Harvey Deitel chapter3 page91 question 3.23
