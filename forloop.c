e0e03044aa6b64f86e5e0c3b7e9f629731f28fc9
Buse <109986380+irembuseozkose@users.noreply.github.com>
2023-04-01 06:40:16 +0300

    Create forloop.c

---
+++ forloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    printf("N 10*N 100*N 1000*N\n");
+    for (N=1;N<=10;N++){
+        printf("%d %d %d %d\n",N,10*N,100*N,1000*N);
+    }
+    return 0;
+}
+// Homework for the Introduction to Computer Programming lesson
+//C: How to Program by Paul and Harvey Deitel chapter3 page92 question 3.25
